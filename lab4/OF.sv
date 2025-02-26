4331e50463ef3b193b6a4d559bdbf07a7b7e14a5
FrankT423 <ftars2@uic.edu>
2025-02-25 19:55:59 -0600

    maybe

--- lab4/OF.sv
+++ lab4/OF.sv
@@ -1,7 +1,11 @@
-module OF(input logic [2:0] C, input logic [31:0] A,B,output logic O);
+module OF(input logic [2:0] C, input logic [31:0] A,B, input logic cout, output logic O);
 logic mo2,mo1,mo3, addof, subof;
 mux2_1_1 m1(mo2,0'b1,(C[0]&C[1]&C[2]),O);//if SLT, OF=1, else more muxing
 mux2_1_1 m2(0'b1,mo3(C[1]),mo2);//if doing add or sub operation (c[1]=1) calculate of,  else of=0
 mux2_1_1 m3(subof,addof,mo3);//c[0]=0-> sub, else add oF calc
+//add y as input!!! but not really
+/*
+need cout from adder, if cout=1, then OF, but only for add and sub operations
 
+*/
 endmodule
