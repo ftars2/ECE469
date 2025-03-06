01198029d5b38baddb76435950b43e78e210d0b6
ftars2 <ftars2@uic.edu>
2025-03-06 17:11:16 -0600

    maybe?

--- lab5/flipR.sv
+++ lab5/flipR.sv
@@ -1,5 +1,5 @@
 module flipR(input logic clk, reset,
-input logic  [3:0] d, rval,
+input logic [3:0] d, rval,
 output logic [3:0] q);
 always_ff @(posedge clk, posedge reset)
     if (reset) q<= rval;
